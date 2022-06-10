a0f5feb1321634e25059ae809fb40ad4a386fa1f
Anderson Araujo <andersonaraujoc1@gmail.com>
2022-06-09 23:25:43 -0300

    atualização
    Atualizado nome da pasta

--- src/components/modules/layouts/CardProdutos/index.tsx
+++ src/components/modules/layouts/CardProdutos/index.tsx
@@ -1,11 +1,59 @@
-import { FC } from 'react';
-import { Container } from './styles';
+import { FC } from "react";
+import {
+  CardImagem,
+  CardCategory,
+  Container,
+  Content,
+  CardNameProduct,
+  ValueAndBuy,
+} from "./styles";
+
+import { useApi } from "../../../../hooks/useApi";
+
+interface IProducts {
+  title: string;
+  category: string;
+  price: string;
+  description: string;
+  image: string;
+  rating: {
+    rate: string;
+  };
+}
 
 const CardProdutos: FC = () => {
+  const { data: products, isFetching } = useApi<IProducts[]>("/products");
+
   return (
-    <Container>
-      <h1>Card do Produto</h1>
-    </Container>
+    <>
+      <Container>
+        {isFetching && <h3>Carregando dados da API...</h3>}
+
+        {products?.map((product) => {
+          return (
+            <Content key={product.title}>
+              <CardCategory>
+                <span>{product.category}</span>
+                <strong>⭐ {product.rating.rate}</strong>
+              </CardCategory>
+
+              <CardImagem>
+                <img src={product.image} alt="" />
+              </CardImagem>
+              <CardNameProduct>
+                <h1>{product.title}</h1>
+              </CardNameProduct>
+
+              <ValueAndBuy>
+                <span>💰 {product.price}</span>
+
+                <button>🛒 Comprar</button>
+              </ValueAndBuy>
+            </Content>
+          );
+        })}
+      </Container>
+    </>
   );
 };
 
--- src/components/modules/layouts/CardProdutos/styles.ts
+++ src/components/modules/layouts/CardProdutos/styles.ts
@@ -1,11 +1,88 @@
 import styled from 'styled-components';
 
+export const Container = styled.main`
+  display: grid;
+  grid-template-columns: repeat(3, 0fr);
+  grid-template-rows: repeat(6, 1fr);
+  grid-column-gap: 40px;
+  justify-content: center;
+  gap: 20px;
 
-export const Container = styled.div`
+  h3 {
+    text-align: center;
+    height: 1000px;
+    width: 1000px;
+  }
+`;
 
-`
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
-export const ContainerItens = styled.div`
+  background: white;
 
-`
+  border: 1px solid;
+  border-radius: 10px;
 
+  max-height: 100%;
+  height: 440px;
+  width: 300px;
+  padding: 10px;
+`;
+
+export const CardCategory = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  span:nth-child(1) {
+    background-color: #0076a3;
+    width: 140px;
+    text-align: center;
+
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+  }
+`;
+
+export const CardImagem = styled.div`
+  display: flex;
+  justify-content: center;
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    height: 250px;
+    width: auto;
+  }
+`;
+
+export const CardNameProduct = styled.div`
+  h1 {
+    font-size: 16px;
+  }
+`;
+
+export const ValueAndBuy = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > span {
+    font-size: 15px;
+    color: #000;
+    font-weight: 700;
+  }
+  > button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: #0076a3;
+    color: #fff;
+    width: 100px;
+    height: 40px;
+    border-radius: 100px;
+  }
+`;
